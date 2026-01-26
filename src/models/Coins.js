f75bb1a0c5423ea8ff068c2d254d27ed541193ff
Tourtelier Eliot <eliot.tourtelier@gmail.com>
2026-01-26 16:53:41 +0100

    création de la fonction onCollides qui sera déplacé dans objects et creation d'un object pour player

--- src/models/Coins.js
+++ src/models/Coins.js
@@ -2,4 +2,35 @@ export class Coins extends Objects {
   constructor() {
     super();
   }
+
+  // onCollide(coinX, coinY, coinW, coinH, persoX, persoY, persoW, persoH) {
+  //   const xEndPerso = persoX + persoW;
+  //   const yEndPerso = persoY + persoH;
+  //   const xEndCoin = coinX + coinW;
+  //   const yEndCoin = coinY + coinH;
+  //   if (
+  //     persoX <= xEndCoin &&
+  //     xEndPerso >= coinX &&
+  //     yEndPerso >= coinY &&
+  //     persoY <= yEndCoin
+  //   ) {
+  //     return true; // OUI, il y a contact
+  //   }
+  //   return false;
+  // }
+
+  onCollide(persoHitbox) {
+    //persoHitbox = {persoX: 50, persoY: 50, endPersoX: 100 --> startX + width, endPersoY: 100 --> startY + height}
+    const endX = this.coordX + this.width;
+    const endY = this.coordY + this.height;
+    if (
+      persoHitbox.persoX < endX &&
+      persoHitbox.endPersoX > this.coordX &&
+      persoHitbox.endPersoY > this.coordY &&
+      persoHitbox.persoY < endY
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
--- src/models/Player.js
+++ src/models/Player.js
@@ -1,4 +1,13 @@
 // création d'une classe
 export class Personnage {
   // constructor()
+
+  getBounds() {
+    return {
+      startX: 0,
+      startY: 0,
+      endX: 0,
+      endY: 0,
+    };
+  }
 }
