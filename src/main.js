b1b94208439b44db53839775f64a59116c9781f9
fefe <felix.parendeau@gmail.com>
2026-01-27 10:33:37 +0100

    add shema of section homePage in comment in index.html

--- src/main.js
+++ src/main.js
@@ -3,29 +3,29 @@ import "./style.css";
 // * Création du carousel
 const body = document.body;
 
-const carousel = document.createElement("div");
-carousel.classList.add("carousel");
+// const carousel = document.createElement("div");
+// carousel.classList.add("carousel");
 
-const divTrack = document.createElement("div");
-divTrack.classList.add("track");
+// const divTrack = document.createElement("div");
+// divTrack.classList.add("track");
 
-const card = document.createElement("div");
-card.classList.add("card");
+// const card = document.createElement("div");
+// card.classList.add("card");
 
-const img = document.createElement("img");
-img.setAttribute("src", "./src/assets/bg.png");
+// const img = document.createElement("img");
+// img.setAttribute("src", "./src/assets/bg.png");
 
-const card2 = document.createElement("div");
-card2.classList.add("card");
+// const card2 = document.createElement("div");
+// card2.classList.add("card");
 
-const img2 = document.createElement("img");
-img2.setAttribute("src", "./src/assets/bg.png");
+// const img2 = document.createElement("img");
+// img2.setAttribute("src", "./src/assets/bg.png");
 
-card2.append(img2);
-card.append(img);
-divTrack.append(card);
-carousel.append(divTrack);
-body.append(carousel);
+// card2.append(img2);
+// card.append(img);
+// divTrack.append(card);
+// carousel.append(divTrack);
+// body.append(carousel);
 
 // * Création du setInterval
 const track = document.querySelector(".track");
