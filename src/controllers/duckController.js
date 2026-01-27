e4889241f5d7e3db844a0630ee86e5c7a3e74331
fefe <felix.parendeau@gmail.com>
2026-01-27 13:56:40 +0100

    remove folder utils and controller

--- src/controllers/duckController.js
+++
@@ -1,5 +0,0 @@
-// import du/des manager(s) et de la/des view(s)
-
-// on a tout centraliser dans le main.js comme si c'etait la view mais celui ci n'est censé n'appelé que le controller qui lui même appelle les views. On construit nos page dans les views et non pas directement dans le main.
-
-//pour faire en sorte de faire que le perso vol il faut faire un addEventListener qui appel un callback présent et défini au préalable dans la class player. La methode appelé dans la class déclanche le le fait que le perso vol (isFlying = true) et un emit se lance. Il faut bien abonné la fonction de fly a un élément dans la view avec le on('nom dans le emit', nomDu Callback)
--- src/main.js
+++ src/main.js
@@ -29,57 +29,57 @@ sectionHomePage.append(titleHomePage, divFlex);
 body.append(sectionHomePage);
 
 // ! Création de la page de jeu
-// const carousel = document.createElement("div");
-// carousel.classList.add("carousel");
-// const carouselQuery = document.querySelector(".carousel");
-// carouselQuery.style.display = "none";
+const carousel = document.createElement("div");
+carousel.classList.add("carousel");
+const carouselQuery = document.querySelector(".carousel");
+carouselQuery.style.display = "none";
 
-// const divTrack = document.createElement("div");
-// divTrack.classList.add("track");
+const divTrack = document.createElement("div");
+divTrack.classList.add("track");
 
-// const card = document.createElement("div");
-// card.classList.add("card");
+const card = document.createElement("div");
+card.classList.add("card");
 
-// const img = document.createElement("img");
-// img.setAttribute("src", "./src/assets/bg.png");
+const img = document.createElement("img");
+img.setAttribute("src", "./src/assets/bg.png");
 
-// const card2 = document.createElement("div");
-// card2.classList.add("card");
+const card2 = document.createElement("div");
+card2.classList.add("card");
 
-// const img2 = document.createElement("img");
-// img2.setAttribute("src", "./src/assets/bg.png");
+const img2 = document.createElement("img");
+img2.setAttribute("src", "./src/assets/bg.png");
 
-// card2.append(img2);
-// card.append(img);
-// divTrack.append(card);
-// carousel.append(divTrack);
-// body.append(carousel);
+card2.append(img2);
+card.append(img);
+divTrack.append(card);
+carousel.append(divTrack);
+body.append(carousel);
 
 // ! Création de skinPage
-// const sectionSkinPage = document.createElement("section");
-// sectionSkinPage.setAttribute("id", "skin-page");
-// const skinPageQuery = document.getElementById("skin-page");
-// skinPageQuery.style.display = "none";
+const sectionSkinPage = document.createElement("section");
+sectionSkinPage.setAttribute("id", "skin-page");
+const skinPageQuery = document.getElementById("skin-page");
+skinPageQuery.style.display = "none";
 
-// const titleSkinPage = document.createElement("h2");
-// titleSkinPage.textContent = "Page des skins";
+const titleSkinPage = document.createElement("h2");
+titleSkinPage.textContent = "Page des skins";
 
-// sectionSkinPage.append(titleSkinPage);
-// body.append(sectionSkinPage);
+sectionSkinPage.append(titleSkinPage);
+body.append(sectionSkinPage);
 
 // ! Création de SettingPage
-// const sectionSettingPage = document.createElement("section");
-// sectionSettingPage.setAttribute("id", "setting-page");
-// const settingPageQuery = document.getElementById("setting-page");
-// settingPageQuery.style.display = "none";
+const sectionSettingPage = document.createElement("section");
+sectionSettingPage.setAttribute("id", "setting-page");
+const settingPageQuery = document.getElementById("setting-page");
+settingPageQuery.style.display = "none";
 
-// const titleSettingPage = document.createElement("h2");
-// titleSettingPage.textContent = "Page des settings";
+const titleSettingPage = document.createElement("h2");
+titleSettingPage.textContent = "Page des settings";
 
-// sectionSettingPage.append(titleSettingPage);
-// body.append(sectionSettingPage);
+sectionSettingPage.append(titleSettingPage);
+body.append(sectionSettingPage);
 
-// * Création du setInterval
+// ! Création du setInterval
 const track = document.querySelector(".track");
 track.innerHTML += track.innerHTML;
 
